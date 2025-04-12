f2f63fbe487b918ae2fcc1737d17e37932243857
TVGneRd <dimas.seledkin.2002@mail.ru>
2025-04-12 21:11:15 +0300

    Исправлены синтаксические ошибки

--- ip_src/Processor/src/Controllers/ControlUnit.vhd
+++ ip_src/Processor/src/Controllers/ControlUnit.vhd
@@ -2,6 +2,7 @@
 LIBRARY IEEE;--! standard library IEEE (Institute of Electrical and Electronics Engineers)
 USE IEEE.std_logic_1164.ALL;--! standard unresolved logic UX01ZWLH-
 USE IEEE.numeric_std.ALL;--! for the signed, unsigned types and arithmetic ops
+USE work.riscv_opcodes_pkg.ALL;
 
 -- -- -- -- Задача блока: -- -- -- --
 -- 1. Выполнять переходы и системные функции
@@ -14,7 +15,7 @@ USE IEEE.numeric_std.ALL;--! for the signed, unsigned types and arithmetic ops
 ENTITY ControlUnit IS
   PORT (
     refclk : IN STD_LOGIC;--! reference clock expect 250Mhz
-    rst    : IN STD_LOGIC--! sync active high reset. sync -> refclk
+    rst    : IN STD_LOGIC;--! sync active high reset. sync -> refclk
 
     opcode : IN riscv_opcode_t;
 
@@ -26,7 +27,7 @@ ENTITY ControlUnit IS
     enable : IN STD_LOGIC;
 
     pc_in  : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-    pc_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    pc_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
   );
 END ENTITY ControlUnit;
 ARCHITECTURE rtl OF ControlUnit IS
