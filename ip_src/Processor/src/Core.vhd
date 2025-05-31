a34f9b8b7d7aeb6229365c608ba91bab68997f5d
TVGneRd <dimas.seledkin.2002@mail.ru>
2025-05-31 17:48:04 +0300

    fix: Поведение при переходах

--- ip_src/Processor/src/Core.vhd
+++ ip_src/Processor/src/Core.vhd
@@ -390,15 +390,29 @@ BEGIN
         result_controller_result  <= (OTHERS => '0');
         result_controller_rd_addr <= (OTHERS => '0');
         pc_jump_flag              <= '0';
+        pc_jump_addr              <= (OTHERS => '0');
       ELSE
         IF execution_control.reg_write = '1' THEN
-          pc_jump_flag              <= '0';
+          pc_jump_flag <= '0';
+          pc_jump_addr <= (OTHERS => '0');
+
           result_controller_enable  <= '1';
           result_controller_result  <= alu_result;
           result_controller_rd_addr <= execution_rd_addr;
+
         ELSIF control_jump = '1' AND (execution_control.branch = '1' OR execution_control.jump = '1') THEN
-          pc_jump_flag <= '1';
           pc_jump_addr <= control_pc_out(11 DOWNTO 0);
+          pc_jump_flag <= '1';
+
+          result_controller_enable  <= '0';
+          result_controller_result  <= (OTHERS => '0');
+          result_controller_rd_addr <= (OTHERS => '0');
+        ELSE
+          result_controller_enable  <= '0';
+          result_controller_result  <= (OTHERS => '0');
+          result_controller_rd_addr <= (OTHERS => '0');
+          pc_jump_flag              <= '0';
+          pc_jump_addr              <= (OTHERS => '0');
         END IF;
       END IF;
     END IF;
